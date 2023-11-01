6bed06a127a1aa0da3b69d94265f833f8bf158ec
Brandon Croft <brandon.croft@gmail.com>
2023-11-01 13:52:44 -0600

    Rebuild after dep bump

--- dist/destroy/index.js
+++ dist/destroy/index.js
@@ -11232,7 +11232,7 @@ function axiosRetry(axios, defaultOptions) {
         }
 
         config.transformRequest = [data => data];
-        onRetry(currentState.retryCount, error, config);
+        yield onRetry(currentState.retryCount, error, config);
         return new Promise(resolve => setTimeout(() => resolve(axios(config)), delay));
       }
 
